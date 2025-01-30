fdeca938b088a4005c7890337ad76d4b003460e9
Claire Mirran <clairemirran@Mac.attlocal.net>
2025-01-30 00:25:13 -0800

    Fix: Update CSS configuration for proper styling in production

--- next.config.js
+++ next.config.js
@@ -9,6 +9,13 @@ const nextConfig = {
   },
   output: 'export',
   trailingSlash: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
