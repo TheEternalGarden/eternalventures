745b935fc1d2ae3fbfdbf65594d2789217454e2e
Claire Mirran <clairemirran@Mac.attlocal.net>
2025-01-30 00:26:56 -0800

    Fix: Simplify CSS configuration and update dependencies

--- next.config.js
+++ next.config.js
@@ -3,19 +3,11 @@
 // Configuration for static site generation
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     unoptimized: true,
   },
   output: 'export',
   trailingSlash: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
-  },
 }
 
 module.exports = nextConfig 
