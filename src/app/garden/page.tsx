bc43856f4f8875dc2d151200cff1c763e2948ab0
ETERNAL LABS <eternal@gardenx.xyz>
2025-02-07 21:56:28 -0800

    fix: switch to img tags for better image display in gallery

--- src/app/garden/page.tsx
+++ src/app/garden/page.tsx
@@ -225,47 +225,53 @@ export default function Garden() {
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full z-0 overflow-x-auto scrollbar-hide">
             <div className="flex gap-8 px-8 min-w-max mx-auto transition-transform duration-300 ease-in-out">
               {/* Sara */}
-              <div 
-                className="relative w-[800px] h-[600px] bg-contain bg-center bg-no-repeat hover:scale-[1.02] transition-transform duration-300"
-                style={{
-                  backgroundImage: `url('/images/garden/sara.jpg')`
-                }}
-              />
+              <div className="relative w-[800px] h-[600px] hover:scale-[1.02] transition-transform duration-300">
+                <img
+                  src="/images/garden/sara.jpg"
+                  alt="Sara"
+                  className="w-full h-full object-contain"
+                />
+              </div>
               {/* Kai */}
-              <div 
-                className="relative w-[800px] h-[600px] bg-contain bg-center bg-no-repeat hover:scale-[1.02] transition-transform duration-300"
-                style={{
-                  backgroundImage: `url('/images/garden/kaicompressed.jpg')`
-                }}
-              />
+              <div className="relative w-[800px] h-[600px] hover:scale-[1.02] transition-transform duration-300">
+                <img
+                  src="/images/garden/kaicompressed.jpg"
+                  alt="Kai"
+                  className="w-full h-full object-contain"
+                />
+              </div>
               {/* Kevin */}
-              <div 
-                className="relative w-[800px] h-[600px] bg-contain bg-center bg-no-repeat hover:scale-[1.02] transition-transform duration-300"
-                style={{
-                  backgroundImage: `url('/images/garden/kevincompressed.jpg')`
-                }}
-              />
+              <div className="relative w-[800px] h-[600px] hover:scale-[1.02] transition-transform duration-300">
+                <img
+                  src="/images/garden/kevincompressed.jpg"
+                  alt="Kevin"
+                  className="w-full h-full object-contain"
+                />
+              </div>
               {/* Refisings */}
-              <div 
-                className="relative w-[800px] h-[600px] bg-contain bg-center bg-no-repeat hover:scale-[1.02] transition-transform duration-300"
-                style={{
-                  backgroundImage: `url('/images/garden/refisings.jpg')`
-                }}
-              />
+              <div className="relative w-[800px] h-[600px] hover:scale-[1.02] transition-transform duration-300">
+                <img
+                  src="/images/garden/refisings.jpg"
+                  alt="Refisings"
+                  className="w-full h-full object-contain"
+                />
+              </div>
               {/* Domino */}
-              <div 
-                className="relative w-[800px] h-[600px] bg-contain bg-center bg-no-repeat hover:scale-[1.02] transition-transform duration-300"
-                style={{
-                  backgroundImage: `url('/images/garden/dominocompressed.jpg')`
-                }}
-              />
+              <div className="relative w-[800px] h-[600px] hover:scale-[1.02] transition-transform duration-300">
+                <img
+                  src="/images/garden/dominocompressed.jpg"
+                  alt="Domino"
+                  className="w-full h-full object-contain"
+                />
+              </div>
               {/* TK */}
-              <div 
-                className="relative w-[800px] h-[600px] bg-contain bg-center bg-no-repeat hover:scale-[1.02] transition-transform duration-300"
-                style={{
-                  backgroundImage: `url('/images/garden/tkcompressed.jpg')`
-                }}
-              />
+              <div className="relative w-[800px] h-[600px] hover:scale-[1.02] transition-transform duration-300">
+                <img
+                  src="/images/garden/tkcompressed.jpg"
+                  alt="TK"
+                  className="w-full h-full object-contain"
+                />
+              </div>
             </div>
           </div>
         </div>
