833a8e61172d198d65cf0d3a2521a929bf566fcd
ETERNAL LABS <eternal@gardenx.xyz>
2025-02-07 23:26:08 -0800

    Add kaicompressed.jpg as fourth image in gallery sequence

--- src/app/garden/page.tsx
+++ src/app/garden/page.tsx
@@ -255,6 +255,16 @@ export default function Garden() {
                   />
                 </div>
               </div>
+              {/* Fourth Image */}
+              <div className="flex-none w-full h-full snap-center flex justify-center items-center">
+                <div className="w-[1000px] h-[750px]">
+                  <img
+                    src="/images/garden/kaicompressed.jpg"
+                    alt="Kai"
+                    className="w-full h-full object-contain"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
