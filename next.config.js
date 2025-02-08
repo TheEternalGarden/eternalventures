4cf4f195b1da8cda4036fe1aee9f528f1b737522
ETERNAL LABS <eternal@gardenx.xyz>
2025-02-07 22:41:53 -0800

    fix: simplify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-// Configuration for static site generation
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [],
-    unoptimized: true,
-  },
   output: 'export',
-  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
-modu
+module.exports = nextConfig 
