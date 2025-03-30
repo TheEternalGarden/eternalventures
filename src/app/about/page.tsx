6acd79b70f1a06e10299e5ce4a250f3ff8f5fe4c
ETERNAL LABS <eternal@gardenx.xyz>
2025-03-29 22:48:44 -0700

    Scale image to fill section height

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -80,12 +80,12 @@ export default function About(): JSX.Element {
       </div>
 
       {/* Second Section */}
-      <div className="h-screen snap-start flex flex-col items-center justify-center bg-black p-8">
-        <div className="relative w-full max-w-[90vw] h-auto">
+      <div className="h-screen snap-start flex flex-col items-center justify-center bg-black">
+        <div className="relative w-full h-screen">
           <img
             src="/images/evbreakdownfinal.jpg"
             alt="EV Breakdown"
-            className="w-full h-auto max-h-[80vh] object-contain"
+            className="w-full h-full object-contain"
             onError={(e) => {
               console.error('Image failed to load:', e);
               const img = e.target as HTMLImageElement;
